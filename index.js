3c6c91250d7cb298381374641085ca06c2537bfd
Sreelekha <117349474+Sreelekha-Sree@users.noreply.github.com>
2023-01-06 11:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const CongratsCard = (props) => {
         <h1 className="head">Congratulations</h1>
     <div className="main-container">
         <img className="img" src={imageurl1} />
-      <h1 className="name" > Shasi B </h1>
+      <h1 className="name" > Sreelekha </h1>
       <p className="desc">Vishnu Institute of Computer Education and Technology, Bhimavaram </p>
         <img className="img1" src={imageurl2} /> 
      </div>
