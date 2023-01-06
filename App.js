f30cd659370e66e758e811575e98a452235db863
Sreelekha <117349474+Sreelekha-Sree@users.noreply.github.com>
2023-01-06 11:48:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import CongratsCard from "./components/UserProfile/coding practice"
 
 const App = () => < CongratsCard
-imageurl1="https://res.cloudinary.com/dbfqymryx/image/upload/v1672916732/IMG_20220415_192511_fedpad.jpg" 
+imageurl1="https://res.cloudinary.com/dbfqymryx/image/upload/v1672918685/2022-02-08-09-38-01-423_tya68w.jpg" 
 imageurl2="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
 export default App
