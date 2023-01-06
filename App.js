b904f804be2edf558ea43696d73036148eee7235
Sreelekha <117349474+Sreelekha-Sree@users.noreply.github.com>
2023-01-06 11:45:00 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,6 @@
+import CongratsCard from "./components/UserProfile/coding practice"
+
+const App = () => < CongratsCard
+imageurl1="https://res.cloudinary.com/dbfqymryx/image/upload/v1672916732/IMG_20220415_192511_fedpad.jpg" 
+imageurl2="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+export default App
